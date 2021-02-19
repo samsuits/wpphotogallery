c69aec47e434acdbd432f7d9726a2eb9f2080802
samsuits <satish.samala@gmail.com>
2021-02-19 18:16:55 +0530

    gitconfig

--- wp-config.php
+++ wp-config.php
@@ -37,6 +37,14 @@ define( 'DB_CHARSET', 'utf8' );
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
+if(isset($_ENV['CLEARDB_DATABASE_URL'])) {
+    $db = parse_url($_ENV['CLEARDB_DATABASE_URL']);
+    define('DB_NAME', trim($db['path'],'/'));
+    define('DB_USER', $db['user']);
+    define('DB_PASSWORD', $db['pass']);
+    define('DB_HOST', $db['host']);
+} 
+
 define('FS_METHOD','direct');
 
 /**#@+
